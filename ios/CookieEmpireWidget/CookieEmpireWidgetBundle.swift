197bb8e4668fefdee4cd4295c2d611ab3a45440f
Kasun Jayawardhana <kasunpradeep.ds@gmail.com>
2024-12-21 23:47:57 +0530

    ios widget bundle

---
+++ ios/CookieEmpireWidget/CookieEmpireWidgetBundle.swift
@@ -0,0 +1,18 @@
+//
+//  CookieEmpireWidgetBundle.swift
+//  CookieEmpireWidget
+//
+//  Created by I O N Groups Pvt Ltd on 2024-12-19.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct CookieEmpireWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        CookieEmpireWidget()
+        CookieEmpireWidgetControl()
+        CookieEmpireWidgetLiveActivity()
+    }
+}
